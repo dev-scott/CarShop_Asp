b049cb6e7a924ae38fc6a1604b96b3479fbe72ee
Dev-scott <sadoscott25@gmail.com>
2023-02-05 12:45:49 +0100

    start

---
+++ CarShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CarShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CarShop.Models;
+
+namespace CarShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
